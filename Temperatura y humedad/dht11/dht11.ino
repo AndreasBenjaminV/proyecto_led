50df50225509c8cc70bb245834ddc0bd58ab1c1b
Andreas Maldito Valero <abenjamin.v@gmail.com>
2017-11-01 23:44:50 -0300

    Add files via upload

---
+++ Temperatura y humedad/dht11/dht11.ino
@@ -0,0 +1,26 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <DHT.h>
+
+int SENSOR = 2;
+int temp, humedad;
+DHT dht(SENSOR, DHT11);
+
+void setup(){
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  humedad = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  Serial.print("Temperatura: ");
+  Serial.print(temp);
+  Serial.print("°C, Humedad: ");
+  Serial.print(humedad);
+  Serial.print("%");
+  Serial.print("\n");
+  delay(500);
+}
