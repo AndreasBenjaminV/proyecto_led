43e75b18f9443b88d50cf2637e64648b2719e9c2
Andreas Maldito Valero <abenjamin.v@gmail.com>
2017-11-07 01:53:53 -0300

    Add files via upload

---
+++ tft_dashboard/tft_dashboard.ino
@@ -0,0 +1,223 @@
+/* www.ngarep.net */
+//Project Arduino Monitoring
+#include <SPI.h>          // f.k. for Arduino-1.5.2
+#include "Adafruit_GFX.h"// Hardware-specific library
+#include <MCUFRIEND_kbv.h>
+MCUFRIEND_kbv tft;
+#include "DHT.h"
+
+#define DHTPIN A8    // what digital pin we're connected to
+
+#define relay 0
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+#define RED2RED 0
+#define GREEN2GREEN 1
+#define BLUE2BLUE 2
+#define BLUE2RED 3
+#define GREEN2RED 4
+#define RED2GREEN 5
+
+uint32_t runTime = -99999;       // time for next update
+int reading1 = 0; // Value to be displayed
+int d = 0; // Variable used for the sinewave test waveform
+boolean alert = 0;
+int8_t ramp = 1;
+int tesmod =0;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  tft.reset(); //Reset LCD to begin
+  uint16_t id = tft.readID();
+    tft.begin(id);
+  tft.setRotation(1); // Set Rotation at 0 degress (default)
+
+tft.fillScreen(BLACK); //Set Background Color with BLACK
+    tft.setCursor (7,208);
+    tft.setTextSize (2);
+    tft.setTextColor (WHITE,BLACK);
+    tft.print ("     Variables basicas       "); 
+
+    tft.setCursor (7,7);
+    tft.setTextSize (1);
+    tft.setTextColor (RED,BLACK);
+    tft.print ("                 SpectrumBender      "); 
+
+//Design Interface (lines)
+    tft.fillRect(0,197,320,4,BLUE);
+    tft.fillRect(0,236,320,4,BLUE);
+    
+    pinMode(relay, OUTPUT);
+    dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+if (millis() - runTime >= 500) { // Execute every 500ms
+    runTime = millis();
+    int xpos = 0, ypos = 10, gap = 4, radius = 52;
+    // Draw a large meter
+    xpos = 320/2 - 150, ypos = 40, gap = 100, radius = 50;
+    ringMeter(t,0,100, xpos,ypos,radius,"CELSIUS",GREEN); // Draw analogue meter
+
+    ringMeter(h,0,100, 115,ypos,radius,"HUMIDITY",GREEN); // Draw analogue meter
+    ringMeter(h,0,100, 220,ypos,radius,"H-GROW",GREEN); // Draw analogue meter
+  
+    }
+
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  Serial.print("Fahrenheight: ");
+  Serial.print(f);
+  Serial.println(" *F\t");
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.println(" %\t");
+  if(t>40) {digitalWrite(relay, HIGH);}
+  else
+  {digitalWrite(relay, LOW);}
+  delay(1000);
+}
+
+// #########################################################################
+//  Draw the meter on the screen, returns x coord of righthand side
+// #########################################################################
+int ringMeter(int value, int vmin, int vmax, int x, int y, int r, char *units, byte scheme)
+{ 
+  x += r; y += r;   // Calculate coords of centre of ring
+  int w = r / 3;    // Width of outer ring is 1/4 of radius 
+  int angle = 150;  // Half the sweep angle of meter (300 degrees)
+  int v = map(value, vmin, vmax, -angle, angle); // Map the value to an angle v
+  byte seg = 3; // Segments are 3 degrees wide = 100 segments for 300 degrees
+  byte inc = 6; // Draw segments every 3 degrees, increase to 6 for segmented ring
+  // Variable to save "value" text colour from scheme and set default
+  int colour = BLUE;
+ 
+  // Draw colour blocks every inc degrees
+  for (int i = -angle+inc/2; i < angle-inc/2; i += inc) {
+    // Calculate pair of coordinates for segment start
+    float sx = cos((i - 90) * 0.0174532925);
+    float sy = sin((i - 90) * 0.0174532925);
+    uint16_t x0 = sx * (r - w) + x;
+    uint16_t y0 = sy * (r - w) + y;
+    uint16_t x1 = sx * r + x;
+    uint16_t y1 = sy * r + y;
+
+    // Calculate pair of coordinates for segment end
+    float sx2 = cos((i + seg - 90) * 0.0174532925);
+    float sy2 = sin((i + seg - 90) * 0.0174532925);
+    int x2 = sx2 * (r - w) + x;
+    int y2 = sy2 * (r - w) + y;
+    int x3 = sx2 * r + x;
+    int y3 = sy2 * r + y;
+
+    if (i < v) { // Fill in coloured segments with 2 triangles
+      switch (scheme) {
+        case 0: colour = RED; break; // Fixed colour
+        case 1: colour = GREEN; break; // Fixed colour
+        case 2: colour = BLUE; break; // Fixed colour
+        case 3: colour = rainbow(map(i, -angle, angle, 0, 127)); break; // Full spectrum blue to red
+        case 4: colour = rainbow(map(i, -angle, angle, 70, 127)); break; // Green to red (high temperature etc)
+        case 5: colour = rainbow(map(i, -angle, angle, 127, 63)); break; // Red to green (low battery etc)
+        default: colour = BLUE; break; // Fixed colour
+      }
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, colour);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, colour);
+      //text_colour = colour;
+    }
+    else // Fill in blank segments
+    {
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, GREEN);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, GREEN);
+    }
+  }
+  // Convert value to a string
+  char buf[10];
+  byte len = 2; if (value > 999) len = 4;
+  dtostrf(value, len, 0, buf);
+  buf[len] = ' '; buf[len] = 0;
+  // Set the text colour to default
+  tft.setTextSize(1);
+
+  if(value>9){
+  tft.setTextColor(colour,BLACK);
+  tft.setCursor(x-25,y-10);tft.setTextSize(3);
+  tft.print(buf);}
+  if(value<10){
+  tft.setTextColor(colour,BLACK);
+  tft.setCursor(x-25,y-10);tft.setTextSize(3);
+  tft.print(buf);}
+
+  tft.setTextColor(WHITE,BLACK);
+  
+  tft.setCursor(x-39,y+75);tft.setTextSize(2);
+  tft.print(units); // Units display
+  
+  // Calculate and return right hand side x coordinate
+  return x + r;
+}
+
+// #########################################################################
+// Return a 16 bit rainbow colour
+// #########################################################################
+unsigned int rainbow(byte value)
+{
+  byte red = 0; 
+  byte green = 0; 
+  byte blue = 0; 
+  byte quadrant = value / 32;
+
+  if (quadrant == 0) {
+    blue = 31;
+    green = 2 * (value % 32);
+    red = 0;
+  }
+  if (quadrant == 1) {
+    blue = 31 - (value % 32);
+    green = 63;
+    red = 0;
+  }
+  if (quadrant == 2) {
+    blue = 0;
+    green = 63;
+    red = value % 32;
+  }
+  if (quadrant == 3) {
+    blue = 0;
+    green = 63 - 2 * (value % 32);
+    red = 31;
+  }
+  return (red << 11) + (green << 5) + blue;
+}
+
+// #########################################################################
+// Return a value in range -1 to +1 for a given phase angle in degrees
+// #########################################################################
+float sineWave(int phase) {
+  return sin(phase * 0.0174532925);
+}
